3cccf7849d19c56ba7e7df6b6b97854df22deb80
unknown <rtallman01@gmail.com>
2023-05-01 13:52:12 -0800

    Separted the plot into four separate graphs using face_wrap function. Relabelled the graphs with appropriate names.

--- scripts/Cumulate_percetages_site.R
+++ scripts/Cumulate_percetages_site.R
@@ -14,7 +14,7 @@ det_dat <- read_csv("CompressedDetections.csv")
 
 # Read in tagging metadata
 tag_dat <- read_csv("2020_Klamath_Tagging_Merged_Full_Database.csv") %>% 
-  filter(is.na(TagEffects)|TagEffects == 0) %>% 
+  filter(is.na(TagEffects)|TagEffects == 0) %>%  # select tag codes that are not tag effects codes
   select(HexID=JSATS, Release_location)
 
 
@@ -33,6 +33,8 @@ tag_dat <- read_csv("2020_Klamath_Tagging_Merged_Full_Database.csv") %>%
 # Rec 200258
 # Rec 200204
 
+
+
 wd_mouth <- det_dat %>% 
   filter(File =="Rec_200203_formatted.csv"|
            File == "Rec_200248_formatted.csv"|
@@ -40,6 +42,8 @@ wd_mouth <- det_dat %>%
            File == "Rec_200204_formatted.csv") %>% 
   mutate(Site_name = "Wood_mouth") %>% 
   select(HexID, StartTime, EndTime, Site_name, File)
+
+
 
 
 # receivers at Williamson River mouth
@@ -55,7 +59,6 @@ wm_mouth <- det_dat %>%
            File == "Rec_200193_formatted.csv") %>% 
   mutate(Site_name = "Williamson_mouth") %>% 
   select(HexID, StartTime, EndTime, Site_name, File)
-
 
 
 # receivers upstream of Link River Dam
@@ -93,9 +96,10 @@ ds_link <- det_dat %>%
            File == "Rec_200216_formatted.csv"
   ) %>% 
   mutate(Site_name = "DS_Link") %>% 
-  select(HexID, StartTime, EndTime, Site_name, File)
+  select(HexID,StartTime, EndTime, Site_name, File)
 
-# bind data backtogether
+
+# bind data back together
 
 d <- rbind(wd_mouth, wm_mouth, us_link, ds_link)
 
@@ -107,7 +111,7 @@ d$HexID <- toupper(d$HexID)
 # filter out any "detections that occurred prior to fish release 04/04/22
 
 d2 <- d %>% 
-  filter(HexID %in% tag_dat$HexID == 1) %>% 
+  filter(HexID %in% tag_dat$HexID) %>% 
   filter(StartTime > "2022-04-03")
 
 
@@ -118,13 +122,45 @@ d3 <- d2 %>%
   group_by(HexID, Site_name)
 
 
+# Determine Total # of detentions per site after filtering for start time and removing tag effects code ids.
+
+wood <- d3 %>% 
+  filter(Site_name == "Wood_mouth")
+length(unique(wood$HexID)) # count the number of total detections at this site
+# 399 tag codes
+
+will <- d3 %>% 
+  filter(Site_name == "Williamson_mouth")
+length(unique(will$HexID))
+# 373 tag codes
+
+link_us <- d3 %>% 
+  filter(Site_name =="US_Link")
+length(unique(link_us$HexID))
+#54 tag codes
+
+link_ds <- d3 %>% 
+  filter(Site_name == "DS_Link")
+length(unique(link_ds$HexID))
+
+# 48 tag codes
+
+
+# Use case when statements to update dataframe with correct number of total detections at a given site. 
+
+d4 <-d3 %>% 
+  mutate(Det_num = case_when(Site_name == "Wood_mouth"~ 399,
+                             Site_name == "Williamson_mouth" ~ 373,
+                             Site_name == "US_Link" ~ 54,
+                             Site_name == "DS_Link" ~ 48))
+
 
 # Organize Data for Cumulative Sums ---------------------------------------
 
 
 d4 <- d3 %>% 
   group_by(HexID, Site_name) %>% 
-  arrange(EndTime) %>% 
+  arrange(StartTime) %>% 
   slice(n()) %>%  # take the last detection at that site for that tag code
   add_count(Site_name) %>%  # tally of the tag code at that site. It should be once. 
   ungroup() %>% 
@@ -132,44 +168,68 @@ d4 <- d3 %>%
   
 
 
-d4 <- d3 %>% 
+d5 <- d4 %>% 
   group_by(HexID, Site_name) %>% 
   arrange(StartTime) %>% 
-  slice(1) %>%  # take the last detection at that site for that tag code
+  slice(1) %>%  # take the first detection at that site for that tag code
   mutate(Date = as.Date(StartTime)) %>%  # remove time stamp of detection and keep the date)
   ungroup() %>% 
   group_by(HexID, Site_name, Date) %>% 
   add_count(Site_name) %>%  # tally of the tag code at that site. It should be once. 
   ungroup() %>% 
-  select(HexID,Site_name, Release_location, Date , n)
+  select(HexID, Det_num, Site_name, Release_location, Date , n)
   
 
-# Calculating Cumulative Sums based on Site Name and Date
+# Calculating Cumulative Sums percentages based on Site Name and Date
 
-det_cum_sum <- d4 %>% 
+det_cum_sum <- d5 %>% 
   group_by(Site_name) %>% 
   arrange(Date) %>% 
-  mutate(cum_sum = cumsum(n))
+  mutate(cum_per = (cumsum(n)/Det_num))
 
+# look up cumulative percent (per)/ (total unique detections)
 
 # Plot the cumulative sums for the different sites
 
-ggplot(det_cum_sum,
-       aes(x= Date, y=cum_sum, group= Site_name, color= Site_name))+
-  geom_step()+
+# reorder site names
+
+det_cum_sum$Site_name <- as.factor(det_cum_sum$Site_name)
+
+det_cum_sum$Site_name <- factor(det_cum_sum$Site_name, levels = c("Wood_mouth","Williamson_mouth", "US_Link", "DS_Link")) 
+
+# remove all other dataframes except the det_cum_sum
+
+rm(list=setdiff(ls(), "det_cum_sum"))
+
+
+# Individual Site Graphs --------------------------------------------------
+
+
+# create new facet labels before plotting
+
+det_cum_sum2 <- det_cum_sum %>% 
+  mutate(Site_name = recode(Site_name,
+                             "Wood_mouth"  = "Wood River Mouth",
+                             "Williamson_mouth" = "Williamson River Mouth",
+                             "US_Link" = "Upstream Link River Dam",
+                             "DS_Link" = "Downstream Link River Dam"))
+
+
+
+
+ggplot(det_cum_sum2,
+       aes(x= Date, y=cum_per, group= Site_name, color= Site_name))+
+  geom_step(size= 1.8)+
+  facet_wrap(~Site_name) + # create four separate plots based on site name
+  ylab("Cummulative Number of Tags Detected") +
   theme_bw() +
-  theme(legend.position = c(.85, 0.2),
+  labs(color = "Site") +
+  scale_color_discrete(labels=c('Wood River', 'Williamson River', 'US Link River', 'DS Link River'))+
+  theme(legend.position = "none",
+        axis.text=element_text(size= 14),
+        axis.title = element_text(size=16),
         panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank())
-   
-  
-
-
-
-
-  
-
-
-
-
+        panel.grid.minor = element_blank(),
+        panel.spacing = unit(2, "lines"),
+        strip.text.x = element_text(size = 15)) # change spacing between facets
 
